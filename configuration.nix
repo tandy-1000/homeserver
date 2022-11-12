8df17dde059b0e55b75d4ee0c7f037e2e7d2b232
tandy1000 <tandy1000@cock.li>
2022-11-12 21:46:37 +0000

    fix imports

--- configuration.nix
+++ configuration.nix
@@ -5,9 +5,6 @@
 
   imports = [
     <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
-  ];
-
-  include = [
     ./modules/network.nix
     ./modules/packages.nix
     ./modules/rkmpp.nix
